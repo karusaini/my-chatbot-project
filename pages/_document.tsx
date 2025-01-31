de58a74a362b6292a0b41b0eefa81b9e9e801110
karusaini <karinasaini06@gmail.com>
2025-01-31 13:13:22 +0530

    favicon and seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>TalkBot AI: Intelligent Chatbot Assistant</title>
+        <meta
+          name="description"
+          content="TalkBot AI provides smart, real-time conversations powered by advanced AI, delivering seamless user interactions and dynamic responses."
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
