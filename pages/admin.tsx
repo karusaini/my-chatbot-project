79b6ce633597ffbd4876cf42834845691f92cc6a
karusaini <karinasaini06@gmail.com>
2025-01-31 12:47:39 +0530

    minor fixes

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,3 +1,22 @@
+import supabase from "@/utils/supabase";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function admin() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (!user) {
+        router.push("/auth");
+      }
+    };
+
+    checkAuth();
+  }, [router]);
+
   return <div>Admin Panel</div>;
 }
